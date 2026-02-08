0189fbccb0e031fc20307624e4f569149e522bdc
ramees-docket <ramees@docketai.com>
2026-02-08 16:29:57 +0530

    fix sha256 for clean-node formula

--- Formula/clean-node.rb
+++ Formula/clean-node.rb
@@ -2,7 +2,7 @@ class CleanNode < Formula
   desc "Find and delete node_modules folders to free disk space"
   homepage "https://github.com/rameessalim/clean-node"
   url "https://github.com/rameessalim/clean-node/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  sha256 "66c8248f04b5766214ebdb1d90e7d68a7a2804f2f640d94cfa348bbe0cfbdb85"
   license "MIT"
 
   def install
